5230c8d1e661037c66cef7c0183fb62a1f5410ae
Eduardo Delgaldo <astiamm@gmail.com>
2013-05-01 15:30:41 -0300

    removed mongo

--- app.rb
+++ app.rb
@@ -1,9 +1,6 @@
 require 'rubygems'
 require 'sinatra'
-require 'mongo'
 require 'json'
-
-DB = Mongo::Connection.new.db("elndb", :pool_size => 5, :timeout => 5)
 
 get '/' do
     haml :index, :locals => {:title => "En Los Nervios"}
