cf144854c99dbffc1c8b7d345c731b8e72eab0da
Eduardo Delgaldo <astiamm@gmail.com>
2013-05-01 19:53:37 -0300

    added sass/compass support

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
 gem 'sinatra'
-gem 'bson_ext'
 gem 'haml'
+gem 'sass'
+gem 'compass'
--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'rubygems'
 require 'sinatra'
-require 'json'
+require 'sass'
+require 'compass'
+
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = '/public/stylesheets'
+  end
+
+  set :haml, { :format => :html5 }
+  set :scss, Compass.sass_engine_options
+end
+
+get '/stylesheets/app.css' do
+  scss :app
+end
 
 get '/' do
-    haml :index, :locals => {:title => "En Los Nervios"}
+    haml :index
 end
