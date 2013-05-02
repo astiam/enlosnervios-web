3a897ba2a3890e63a8b140765f342d4358d50c1c
Eduardo Delgaldo <astiamm@gmail.com>
2013-05-02 17:54:53 -0300

    general overhaul

--- app.rb
+++ app.rb
@@ -1,21 +1,5 @@
 require 'rubygems'
 require 'sinatra'
-require 'sass'
-require 'compass'
-
-configure do
-  Compass.configuration do |config|
-    config.project_path = File.dirname(__FILE__)
-    config.sass_dir = '/public/stylesheets'
-  end
-
-  set :haml, { :format => :html5 }
-  set :scss, Compass.sass_engine_options
-end
-
-get '/stylesheets/app.css' do
-  scss :app
-end
 
 get '/' do
     haml :index
---
+++ public/javascripts/app.js
@@ -0,0 +1,1 @@
+$('#start').backstretch('../images/bg.jpg');
